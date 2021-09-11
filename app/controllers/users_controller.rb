aeaf8aca1893f583ef4bc8e16200918f23215d41
lion-oldriver <w879443a72cj2i@gmail.com>
2021-09-11 10:17:25 +0000

    [update]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,7 +16,7 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:name,:profile_image,:introduction)
+    params.require(:user).permit(:name, :profile_image, :introduction)
   end
 
 end
