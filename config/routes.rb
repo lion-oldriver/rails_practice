17019e4d7aa0e2e3dfadaae903aa48c2ef2298d6
lion-oldriver <w879443a72cj2i@gmail.com>
2021-09-13 08:25:33 +0000

    [update]

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
+  }
+
   get "about" => "homes#about"
   get "search" => "searches#search"
   resources :users, only:[:show,:edit,:update] do
