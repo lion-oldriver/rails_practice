6da699b7c5a6b777c1aab4edc02e5b8cd0815e96
lion-oldriver <w879443a72cj2i@gmail.com>
2021-09-28 09:18:34 +0000

    [google_map]

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -17,7 +17,9 @@ class PlacesController < ApplicationController
 
   def show
     @place = Place.find(params[:id])
+    @places = @place.nearbys(1, units: :km)
     gon.place = @place
+    gon.places = @places
   end
 
   private
